7aed472c0d6ae057831f80aa4455881bd1e552ba
vaishnavisingireddy <145335344+vaishnavisingireddy@users.noreply.github.com>
2024-02-26 18:09:55 +0530

    Add files via upload

---
+++ task4/script.js
@@ -0,0 +1,20 @@
+alert("fill the form");
+
+let number1 = 10;
+let number2 = 20;
+let name = "vaishnavi";
+let char = "V";
+let x = true;
+let number3 = 99.7;
+
+console.log(number1);
+console.log(number2);
+console.log(name);
+console.log(char);
+console.log(x);
+
+console.log(typeof(name));
+console.log(typeof(char));
+console.log(typeof(number1));
+console.log(typeof(x));
+console.log(typeof(number3));
